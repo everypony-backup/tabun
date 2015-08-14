c2eee02938d8d45f753d4e2d95e35d3ff054c489
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2015-08-15 02:11:54 +0300

    Custom parser for some tags

--- engine/modules/text/Text.class.php
+++ engine/modules/text/Text.class.php
@@ -120,19 +120,47 @@ class ModuleText extends Module {
 		/**
 		 * youtube.com
 		 */
-		$sText = preg_replace('/<video>http:\/\/(?:www\.|)youtube\.com\/watch\?v=([a-zA-Z0-9_\-]+)(&.+)?<\/video>/Ui', '<iframe width="560" height="315" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>', $sText);
+
+		$y_video_pattern = "/<video>(?:https?:\/\/)?(?:(?:www\.))?(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})(?:\?(?:t|start)=((?:[0-9]{1,10}[hms]?){1,4}))?<\/video>/i";
+		preg_match($y_video_pattern, $sText, $output_array);
+
+		if (count($output_array) == 2) {
+			$y_tpl = '<iframe width="560" height="310" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>';
+		} elseif (count($output_array) == 3 ) {
+			$y_tpl = '<iframe width="560" height="310" src="http://www.youtube.com/embed/$1?start=$2" frameborder="0" allowfullscreen></iframe>';
+		}
+		$sText = preg_replace($y_video_pattern, $y_tpl, $sText);
 		/**
 		 * vimeo.com
-		 */
-		$sText = preg_replace('/<video>http:\/\/(?:www\.|)vimeo\.com\/(\d+).*<\/video>/i', '<iframe src="http://player.vimeo.com/video/$1" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>', $sText);
+		*/
+		$v_video_pattern = '/<video>(?:https?:\/\/)?(?:www\.)?vimeo\.com\/(\d+).*<\/video>/i';
+		$v_tpl = '<iframe src="http://player.vimeo.com/video/$1" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+		$sText = preg_replace($v_video_pattern, $v_tpl, $sText);
+
 		/**
-		 * rutube.ru
-		 */
-		$sText = preg_replace('/<video>http:\/\/(?:www\.|)rutube\.ru\/tracks\/(\d+)\.html.*<\/video>/Ui', '<OBJECT width="470" height="353"><PARAM name="movie" value="http://video.rutube.ru/$1"></PARAM><PARAM name="wmode" value="window"></PARAM><PARAM name="allowFullScreen" value="true"></PARAM><EMBED src="http://video.rutube.ru/$1" type="application/x-shockwave-flash" wmode="window" width="470" height="353" allowFullScreen="true" ></EMBED></OBJECT>', $sText);
+		* dailymotion.com
+		*/
+
+		$d_video_pattern = "/(<video>)(?:https?:\/\/)?(?:www\.)?dailymotion.com\/video\/(\w+)(?:_.*?)?(<\/video>)/ui";
+		$d_video_pattern_short = "/(<video>)(?:https?:\/\/)?(?:www\.)?dai.ly\/(\w+)(?:_.*?)?(<\/video>)/ui";
+		$d_tpl = '<iframe frameborder="0" width="560" height="315" src="http://www.dailymotion.com/embed/video/$2" allowfullscreen></iframe>';
+		$sText = preg_replace($d_video_pattern, $d_tpl, $sText);
+		$sText = preg_replace($d_video_pattern_short, $d_tpl, $sText);
+
 		/**
-		 * video.yandex.ru
-		 */
-		$sText = preg_replace('/<video>http:\/\/video\.yandex\.ru\/users\/([a-zA-Z0-9_\-]+)\/view\/(\d+).*<\/video>/i', '<object width="467" height="345"><param name="video" value="http://video.yandex.ru/users/$1/view/$2/get-object-by-url/redirect"></param><param name="allowFullScreen" value="true"></param><param name="scale" value="noscale"></param><embed src="http://video.yandex.ru/users/$1/view/$2/get-object-by-url/redirect" type="application/x-shockwave-flash" width="467" height="345" allowFullScreen="true" scale="noscale" ></embed></object>', $sText);
+		* coub.com
+		*/
+		$c_video_pattern = "/(<video>)(?:https?:\/\/)?(?:www\.)?coub.com\/view\/(\w+)(<\/video>)/ui";
+		$c_tpl = '<iframe src="http://coub.com/embed/$2?muted=false&autostart=false&originalSize=false&hideTopBar=true&noSiteButtons=true&startWithHD=false" allowfullscreen="true" frameborder="0" width="400" height="400"></iframe>';
+		$sText = preg_replace($c_video_pattern, $c_tpl, $sText);
+		/**
+		* rutube.ru
+		*/
+
+		$r_video_pattern = "/(<video>)(?:https?:\/\/)?(?:www\.)?rutube.ru\/video\/(\w+)\/?(<\/video>)/ui";
+		$r_tpl = '<iframe src="http://rutube.ru/video/embed/$2" allowfullscreen="true" frameborder="0" width="560" height="315"></iframe>';
+		$sText = preg_replace($r_video_pattern, $r_tpl, $sText);
+
 		return $sText;
 	}
 	/**
@@ -145,61 +173,9 @@ class ModuleText extends Module {
 		if (!is_string($sText)) {
 			return '';
 		}
-		$sResult=$this->FlashParamParser($sText);
-		$sResult=$this->JevixParser($sResult);
+		$sResult=$this->JevixParser($sText);
 		$sResult=$this->VideoParser($sResult);
-		$sResult=$this->CodeSourceParser($sResult);
 		return $sResult;
-	}
-	/**
-	 * Заменяет все вхождения короткого тега <param/> на длиную версию <param></param>
-	 * Заменяет все вхождения короткого тега <embed/> на длиную версию <embed></embed>
-	 *
-	 * @param string $sText Исходный текст
-	 * @return string
-	 */
-	protected function FlashParamParser($sText) {
-		if (preg_match_all("@(<\s*param\s*name\s*=\s*(?:\"|').*(?:\"|')\s*value\s*=\s*(?:\"|').*(?:\"|'))\s*/?\s*>(?!</param>)@Ui",$sText,$aMatch)) {
-			foreach ($aMatch[1] as $key => $str) {
-				$str_new=$str.'></param>';
-				$sText=str_replace($aMatch[0][$key],$str_new,$sText);
-			}
-		}
-		if (preg_match_all("@(<\s*embed\s*.*)\s*/?\s*>(?!</embed>)@Ui",$sText,$aMatch)) {
-			foreach ($aMatch[1] as $key => $str) {
-				$str_new=$str.'></embed>';
-				$sText=str_replace($aMatch[0][$key],$str_new,$sText);
-			}
-		}
-		/**
-		 * Удаляем все <param name="wmode" value="*"></param>
-		 */
-		if (preg_match_all("@(<param\s.*name=(?:\"|')wmode(?:\"|').*>\s*</param>)@Ui",$sText,$aMatch)) {
-			foreach ($aMatch[1] as $key => $str) {
-				$sText=str_replace($aMatch[0][$key],'',$sText);
-			}
-		}
-		/**
-		 * А теперь после <object> добавляем <param name="wmode" value="opaque"></param>
-		 * Решение не фантан, но главное работает :)
-		 */
-		if (preg_match_all("@(<object\s.*>)@Ui",$sText,$aMatch)) {
-			foreach ($aMatch[1] as $key => $str) {
-				$sText=str_replace($aMatch[0][$key],$aMatch[0][$key].'<param name="wmode" value="opaque"></param>',$sText);
-			}
-		}
-		return $sText;
-	}
-	/**
-	 * Подсветка исходного кода
-	 *
-	 * @param string $sText Исходный текст
-	 * @return mixed
-	 */
-	public function CodeSourceParser($sText) {
-		$sText=str_replace("<code>",'<pre class="prettyprint"><code>',$sText);
-		$sText=str_replace("</code>",'</code></pre>',$sText);
-		return $sText;
 	}
 	/**
 	 * Производить резрезание текста по тегу cut.
@@ -243,7 +219,7 @@ class ModuleText extends Module {
 	 * <ls user="admin" />
 	 * </pre>
 	 *
-	 * @param string $sTag	Тег на ктором сработал колбэк
+	 * @param string $sTag	Тег на котором сработал колбэк
 	 * @param array $aParams Список параметров тега
 	 * @return string
 	 */
@@ -256,5 +232,15 @@ class ModuleText extends Module {
 		}
 		return $sText;
 	}
+        /**
+        * Смайлопак Табуна
+        */
+        public function CallbackTagSmp($sTag, $aParams) {
+                $sText='';
+                if (isset($aParams['id']) && isset($aParams['name'])) {
+                        $sText.="<img class=\"smp\" src=\"//files.everypony.ru/smiles/".$aParams['name']."/".$aParams['id'].".gif\" />";
+                }
+			return $sText;
+        }
 }
 ?>
