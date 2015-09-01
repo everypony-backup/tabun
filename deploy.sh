0f3807fe18b27121176c27b54ddf35337bcd23cc
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2015-09-02 00:20:08 +0300

    Split deploy environments. Fix #80

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ Usage:
         --chroot /home/ep/tabun.everypony.ru \
         --static /home/ep/cdn.everypony.ru/www/static \
         --app-perms ep_tabun:ep_tabun \
-        --static-perms ep:ep
+        --static-perms ep:ep \
+        --environment production
 
 Options:
    -h, --help           Show this message and exit
@@ -20,6 +21,7 @@ Options:
    -s, --static         Path to static dir
    -a, --app-perms      App files permissions
    -p, --static-perms   Static files permissions
+   -e, --environment    Deploy type (trunk or production)
 EOT
 exit 0;
 }
@@ -42,8 +44,13 @@ deploy(){
         APP_VER=$(git rev-parse --short HEAD)
     fi
     echo "Build static"
-    npm run-script webpack:production
-    local STATIC_VER=$(ls -1 --color=never static/)
+    if [ ${ENV_TYPE} == 'production' ]; then
+        npm run-script webpack:production
+        STATIC_VER=$(ls -1 --color=never static/)
+    else
+        npm run-script webpack:trunk
+        STATIC_VER='trunk'
+    fi
 
     echo "Build i10n files"
     find locale -name "*.po" -execdir msgfmt messages.po -o messages.mo \;
@@ -55,6 +62,10 @@ deploy(){
     echo "Clean smarty pre-compiled templates"
     rm -rf ${CHROOT_PATH}/var/smarty/*
 
+    if [ ${ENV_TYPE} != 'production' ]; then
+        echo "Clean old static from trunk"
+        rm -r ${STATIC_PATH}/trunk/*
+    fi
     echo "Deploy static, version: ${STATIC_VER}"
     rsync -au static/ ${STATIC_PATH}
     cp -r frontend/images/local/ ${STATIC_PATH}
@@ -92,13 +103,18 @@ do
         -s|--static) STATIC_PATH=$2; shift;;
         -a|--app-perms) APP_PERMS=$2; shift;;
         -p|--static-perms) STATIC_PERMS=$2; shift;;
+        -e|--environment) ENV_TYPE=$2; shift;;
         -h|--help) usage;;
         *) break;;
     esac
     shift
 done
 
-if [ -z ${CHROOT_PATH+x} ] || [ -z ${STATIC_PATH+x} ] || [ -z ${APP_PERMS+x} ] || [ -z ${STATIC_PERMS+x} ]; then
+if [ -z ${CHROOT_PATH+x} ] ||
+    [ -z ${STATIC_PATH+x} ] ||
+    [ -z ${APP_PERMS+x} ] ||
+    [ -z ${STATIC_PERMS+x} ] ||
+    [ -z ${ENV_TYPE+x} ]; then
     usage
 else
     deploy
