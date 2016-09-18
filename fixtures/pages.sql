f19bc5dbfbc339d0e3c29af69b0f55aa72153c57
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2016-09-18 17:35:16 +0300

    Add page fixtures (#14)

---
+++ fixtures/pages.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE ls_page;
+INSERT INTO ls_page VALUES
+  (1, null, 'faq', 'faq', 'FAQ', 'TODO', '2016-09-13 22:26:46', null, '', '', 1, 1, 1, 0),
+  (2, null, 'rules', 'rules', 'Правила', 'TODO', '2016-09-13 22:26:46', null, null, null, 1, 1, 2, 0)
+;
