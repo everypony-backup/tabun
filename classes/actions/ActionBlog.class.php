0fe778e207b13b42f76a034e017e2dcdf443a9c1
Farxial <farxial@gmail.com>
2016-09-18 19:58:22 +0300

    Use parser flags in blogs processing (crete/update). Also affects comments in the same way

--- classes/actions/ActionBlog.class.php
+++ classes/actions/ActionBlog.class.php
@@ -206,7 +206,7 @@ class ActionBlog extends Action {
 		/**
 		 * Парсим текст на предмет разных ХТМЛ тегов
 		 */
-		$sText=$this->Text_Parser(getRequestStr('blog_description'));
+		$sText=$this->Text_Parser(getRequestStr('blog_description'), ModuleText::ACT_CREATE);
 		$oBlog->setDescription($sText);
 		$oBlog->setType(getRequestStr('blog_type'));
 		$oBlog->setDateAdd(date("Y-m-d H:i:s"));
@@ -307,7 +307,7 @@ class ActionBlog extends Action {
 			/**
 			 * Парсим описание блога на предмет ХТМЛ тегов
 			 */
-			$sText=$this->Text_Parser(getRequestStr('blog_description'));
+			$sText=$this->Text_Parser(getRequestStr('blog_description'), ModuleText::ACT_UPDATE);
 			$oBlog->setDescription($sText);
 			/**
 			 * Сбрасываем кеш, если поменяли тип блога
@@ -1001,7 +1001,7 @@ class ActionBlog extends Action {
 		/**
 		 * Проверяем текст комментария
 		 */
-		$sText=$this->Text_Parser(getRequestStr('comment_text'));
+		$sText=$this->Text_Parser(getRequestStr('comment_text'), ModuleText::ACT_CREATE);
 		if (!func_check($sText,'text',2,Config::Get('module.comment.comment_max_length'))) {
 			$this->Message_AddErrorSingle($this->Lang_Get('topic_comment_add_text_error'),$this->Lang_Get('error'));
 			return;
