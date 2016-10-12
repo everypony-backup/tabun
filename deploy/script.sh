59e7dc618668b83bbde21e5e5a16569ad1aac3de
Andriy Kushnir (Orhideous) <me@orhideous.name>
2016-10-13 02:40:28 +0300

    Travis probe

---
+++ deploy/script.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -ex
+
+CONTAINERS=( redis )
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -p|--project)       PROJECT=$2; shift;;
+        -d|--destination)   DESTINATION=$2; shift;;
+        -c|--containers)    CONTAINERS=$2; shift;;
+        -s|--server)        SERVER=$2; shift;;
+        -P|--port)          PORT=$2; shift;;
+        -u|--user)          USER=$2; shift;;
+        -h|--help)          usage;;
+        *)                  break;;
+    esac
+    shift
+done
+
+VAGGA=${VAGGA:-vagga}
+USER=${USER:-deploy}
+PORT=${PORT:-22}
+
+usage(){
+cat <<'EOT'
+Deploy script
+
+Usage:
+    ./script.sh \
+        --project tabun-trunk \
+        --destination /srv/images \
+        --server staging.everypony.ru \
+        --containers "redis app python mysql"
+
+Options:
+   -p, --project        Project instance
+   -d, --destination    Path to images on server
+   -s, --server         Server to deploy
+   -P, --port           SSH port
+   -u, --user           SSH user
+   -h, --help           Show this help
+EOT
+exit 0;
+}
+
+deploy(){
+    echo "$PROJECT will be deployed on $SERVER to $DESTINATION"
+    echo
+    echo "Containers:"
+    for CONTAINER in ${CONTAINERS}; do
+        echo "    > $CONTAINER"
+    done
+}
+
+if  [ -z ${PROJECT+x} ] ||
+    [ -z ${SERVER+x} ] ||
+    [ -z ${CONTAINERS+x} ] ||
+    [ -z ${DESTINATION+x} ]; then
+    usage
+else
+    deploy
+fi
