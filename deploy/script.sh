839bcc3219f0d10cbf7261987fe3df51507a56ad
Andriy Kushnir (Orhideous) <me@orhideous.name>
2016-10-13 03:20:31 +0300

    Add dry-run parameter

--- deploy/script.sh
+++ deploy/script.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-CONTAINERS=( redis )
-
 while [ $# -gt 0 ]
 do
     case "$1" in
@@ -13,6 +11,7 @@ do
         -s|--server)        SERVER=$2; shift;;
         -P|--port)          PORT=$2; shift;;
         -u|--user)          USER=$2; shift;;
+        -x|--dry-run)       DRY_RUN=true; ;;
         -h|--help)          usage;;
         *)                  break;;
     esac
@@ -21,6 +20,7 @@ done
 
 VAGGA=${VAGGA:-vagga}
 PORT=${PORT:-22}
+DRY_RUN=${DRY_RUN:-false}
 
 usage(){
 cat <<'EOT'
@@ -45,13 +45,55 @@ EOT
 exit 0;
 }
 
+sync_container() {
+    local NAME="$1"
+    local CONTAINER_NAME=${NAME}-deploy
+
+    # Build container
+    ${VAGGA} _build ${CONTAINER_NAME}
+
+    # Get version
+    VERSION="$($VAGGA _version_hash --short $CONTAINER_NAME)"
+
+    # Copy image to server
+    rsync -a \
+        --checksum \
+        -e "ssh -p $PORT" \
+        --link-dest=${DESTINATION}/${PROJECT}/${CONTAINER_NAME}.latest/ \
+        .vagga/${CONTAINER_NAME}/ \
+        ${USER}@${SERVER}:${DESTINATION}/${PROJECT}/${CONTAINER_NAME}.${VERSION}
+
+    # Link as latest image
+    ssh ${USER}@${SERVER} -p ${PORT} ln -sfn ${CONTAINER_NAME}.${VERSION} ${DESTINATION}/${PROJECT}/${CONTAINER_NAME}.latest
+
+    # Add to config
+    cat <<END | ssh ${USER}@${SERVER} -p ${PORT} tee -a ${DESTINATION}/${PROJECT}/config.yaml
+${NAME}:
+    kind: Daemon
+    instances: 1
+    config: /lithos/${NAME}.yaml
+    image: ${CONTAINER_NAME}.${VERSION}
+END
+}
+
 deploy(){
-    echo "$PROJECT will be deployed on $SERVER to $DESTINATION"
-    echo
-    echo "Containers:"
+    # Create dir, if neccessary
+    ssh ${USER}@${SERVER} -p ${PORT} mkdir -p ${DESTINATION}/${PROJECT}
+
+    # Remove old config
+    ssh ${USER}@${SERVER} -p ${PORT} rm -f ${DESTINATION}/${PROJECT}/config.yaml
+
+    # Sync each container
     for CONTAINER in ${CONTAINERS}; do
-        echo "    > $CONTAINER"
+        sync_container ${CONTAINER}
     done
+
+    # Switch to new config
+    if [ "$DRY_RUN" = "true" ]; then
+        echo "Skipped version switch"
+    else
+        ssh -t ${USER}@${SERVER} -p ${PORT} sudo lithos_switch ${PROJECT} ${DESTINATION}/${PROJECT}/config.yaml
+    fi
 }
 
 if  [ -z ${PROJECT+x} ] ||
