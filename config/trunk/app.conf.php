64764cee1987c794600c11dbe2c2e10d7a7a68ff
Andriy Kushnir (Orhideous) <me@orhideous.name>
2016-10-15 22:02:50 +0300

    Add host-port for celery bindings

--- config/trunk/app.conf.php
+++ config/trunk/app.conf.php
@@ -23,6 +23,8 @@ $config['misc']['debug'] = true;
 $config['misc']['twicher']['url'] = 'http://127.0.0.1:5000/quotes/twitchy';
 $config['sys']['elastic']['hosts'] = ["127.0.0.1"];
 
+$config['sys']['celery']['host'] = '127.0.0.1';
+$config['sys']['celery']['port'] = 6579;
 $config['sys']['celery']['db'] = 1;
 
 return $config;
