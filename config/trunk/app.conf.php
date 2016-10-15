788a52c3133a269381673d801f7ddb6ca1008ddf
Andriy Kushnir (Orhideous) <me@orhideous.name>
2016-10-15 22:19:26 +0300

    Use proper redis instance for caching

--- config/trunk/app.conf.php
+++ config/trunk/app.conf.php
@@ -1,5 +1,4 @@
 <?php
-$config['path']['root']['web'] = '//' . $_SERVER['HTTP_HOST'];
 $config['db']['params']['host'] = '127.0.0.1';
 $config['db']['params']['port'] = '4406';
 $config['db']['params']['user'] = 'tabun_trunk';
@@ -10,21 +9,33 @@ $config['db']['tables']['engine'] = 'InnoDB';
 $config['db']['table']['prefix'] = 'ls_';
 
 $config['path']['offset_request_url'] = '0';
-$config['sys']['logs']['dir'] = '/log';
-
 $config['path']['uploads']['storage'] = '/storage';
 $config['path']['uploads']['url'] = '//cdn.everypony.ru/tabun-trunk-storage';
 $config['path']['static']['url'] = '//cdn.everypony.ru/tabun-trunk-static';
-
 $config['path']['smarty']['compiled'] = '/tmp/smarty/compiled';
 $config['path']['smarty']['cache'] = '/tmp/smarty/cache';
 
-$config['misc']['debug'] = true;
-$config['misc']['twicher']['url'] = 'http://127.0.0.1:5000/quotes/twitchy';
 $config['sys']['elastic']['hosts'] = ["127.0.0.1"];
 
 $config['sys']['celery']['host'] = '127.0.0.1';
 $config['sys']['celery']['port'] = 6579;
 $config['sys']['celery']['db'] = 1;
+
+$config['sys']['cache']['servers'] = [
+    [
+        'host' => '127.0.0.1',
+        'port' => 6579,
+        'dbindex' => 4,
+    ],
+];
+
+$config['sys']['mail']['from_email'] = 'tabun-trunk@everypony.info';
+$config['sys']['mail']['from_name'] = 'Tabun [trunk]';
+
+$config['sys']['logs']['dir'] = '/log';
+
+$config['misc']['debug'] = true;
+$config['misc']['twicher']['url'] = 'http://127.0.0.1:5000/quotes/twitchy';
+
 
 return $config;
