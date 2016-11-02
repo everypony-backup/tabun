ac7f79dc7d79a1f3f0535ba0b04c6d563c5e9fd9
zluca <osam@tut.by>
2016-11-02 14:35:19 +0300

    fix for hotfix. #66

--- classes/modules/comment/Comment.class.php
+++ classes/modules/comment/Comment.class.php
@@ -649,7 +649,7 @@ class ModuleComment extends Module {
 		$oViewerLocal=$this->Viewer_GetLocalViewer();
 		$oViewerLocal->Assign('oUserCurrent',$this->User_GetUserCurrent());
 		$oViewerLocal->Assign('bOneComment',true);
-		$oViewerLocal->Assign('iAuthorId', $oComment->getTarget()->getUserId());
+		$oViewerLocal->Assign('iAuthorId', $aComments[0]->getTarget()->getUserId());
 		$oViewerLocal->Assign('sAuthorNotice', $aLang.topic_author);
 		if($sTargetType!='topic') {
 			$oViewerLocal->Assign('bNoCommentFavourites',true);
