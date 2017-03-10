2f2d12f34636f8ad206e3f3d47b5a5c6fca6d950
Farxial <farxial@gmail.com>
2017-03-10 16:40:00 +0300

    vote_list / Fix vote scalar

--- classes/actions/ActionAjax.class.php
+++ classes/actions/ActionAjax.class.php
@@ -1248,7 +1248,7 @@ class ActionAjax extends Action {
 			$aResult[] = array(
 				'voterName' => $bShowUser ? $oUser->getLogin() : null,
 				'voterAvatar' => $bShowUser ? $oUser->getProfileAvatarPath() : null,
-				'value' => (float) $oVote->getDirection(),
+				'value' => (float) $oVote->getValue(),
 				'date' => (string) $oVote->getDate().'+03:00',
 			);
 		}
