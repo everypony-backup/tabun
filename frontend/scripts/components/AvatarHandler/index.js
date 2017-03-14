2f775b170818b7840767174a855983e2a9a58633
Andriy Kushnir (Orhideous) <me@orhideous.name>
2017-03-14 02:35:16 +0200

    Functional modal window

--- frontend/scripts/components/AvatarHandler/index.js
+++ frontend/scripts/components/AvatarHandler/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import autobind from 'autobind-decorator'
+
+import Modal from 'components/Modal';
 import ImageUpload from './subcomponents';
 
-export default function (props) {
-    return <ImageUpload
-        handleChange={(e) => {
-            debugger;
-        }}
-    />
+
+export default class AvatarHandler extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            img: null,
+            croppedImg: null,
+            cropperOpen: false,
+        };
+    }
+
+    @autobind
+    handleFileChange(dataURI) {
+        this.setState({
+            img: dataURI,
+            cropperOpen: true
+        });
+    }
+
+    @autobind
+    handleRequestHide() {
+        this.setState({cropperOpen: false});
+    }
+
+    render() {
+        return <div>
+            <ImageUpload handleChange={this.handleFileChange} />
+            <Modal
+                header="upload_avatar" // TODO: Add translation
+                onRequestClose={this.handleRequestHide}
+                isOpen={this.state.cropperOpen}
+            >
+                <img src={this.state.img} />  // TODO: change to croppedImg later, this is PoC
+            </Modal>
+        </div>
+    }
 }
