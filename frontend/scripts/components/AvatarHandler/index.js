db5ffc9e0f3d7a7698135d98bb903ad14a9771eb
Andriy Kushnir (Orhideous) <me@orhideous.name>
2017-03-14 03:26:33 +0200

    PoC cropper

--- frontend/scripts/components/AvatarHandler/index.js
+++ frontend/scripts/components/AvatarHandler/index.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import autobind from 'autobind-decorator'
 
+import AvatarEditor from 'react-avatar-editor'
+
 import Modal from 'components/Modal';
 import ImageUpload from './subcomponents';
 
 
 export default class AvatarHandler extends React.Component {
+    static editor = null;
+
     constructor(props) {
         super(props);
         this.state = {
-            img: null,
+            sourceImg: null,
             croppedImg: null,
-            cropperOpen: false,
+            editorOpened: false,
         };
+    }
+
+    @autobind
+    setEditorRef(editor) {
+        if (editor) this.editor = editor;
     }
 
     @autobind
     handleFileChange(dataURI) {
         this.setState({
-            img: dataURI,
-            cropperOpen: true
+            sourceImg: dataURI,
+            editorOpened: true
         });
     }
 
     @autobind
     handleRequestHide() {
-        this.setState({cropperOpen: false});
+        this.setState({editorOpened: false});
+    }
+
+    @autobind
+    handleSave() {
+        const img = this.editor.getImageScaledToCanvas().toDataURL();
+        this.setState({croppedImg: img});
     }
 
     render() {
@@ -34,9 +49,22 @@ export default class AvatarHandler extends React.Component {
             <Modal
                 header="upload_avatar" // TODO: Add translation
                 onRequestClose={this.handleRequestHide}
-                isOpen={this.state.cropperOpen}
+                isOpen={this.state.editorOpened}
             >
-                <img src={this.state.img} />  // TODO: change to croppedImg later, this is PoC
+                <AvatarEditor
+                    ref={this.setEditorRef}
+                    image={this.state.sourceImg}
+                    width={320}
+                    height={320}
+                    border={50}
+                    color={[255, 255, 255, 0.55]}
+                    scale={1.2}
+                    rotate={0}
+
+                    onSave={this.handleSave}
+                />
+                <input type="button" onClick={this.handleSave} value="Preview" />
+                <img src={this.state.croppedImg} />
             </Modal>
         </div>
     }
