d6ed10a60660589e3e87587873cda5d83b85bcb1
Vladimir Avchenov <vladimir.lsk.cool@gmail.com>
2021-11-25 10:01:30 +0300

    Overriding migration file names

--- docker/mariadb/copy_fixtures.sh
+++ docker/mariadb/copy_fixtures.sh
@@ -1,27 +1,11 @@
 #!/bin/sh
 
-filenum=0
-filestr=
-numcounts=0
-gennum () {
-	filestr=$filenum
-	numcounts=1
-	while [ $numcounts -lt 4 ];
-	do
-		filestr="0$filestr"
-		numcounts=$((numcounts+1))
-	done
-	filenum=$((filenum+1))
-}
-
 for file in $(ls /app/fixtures/migrations);
 do
-	gennum
-	cp "/app/fixtures/migrations/$file" "/docker-entrypoint-initdb.d/$filestr-$file"
+	cp "/app/fixtures/migrations/$file" "/docker-entrypoint-initdb.d/0100-$file"
 done
 
 for file in $(ls /app/fixtures/data);
 do
-	gennum
-	cp "/app/fixtures/data/$file" "/docker-entrypoint-initdb.d/$filestr-$file"
+	cp "/app/fixtures/data/$file" "/docker-entrypoint-initdb.d/0200-$file"
 done
