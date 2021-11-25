b8592a32e61f3f927c651cad781d3e4d10a6eb0f
Vladimir Avchenov <vladimir.lsk.cool@gmail.com>
2021-11-25 22:31:18 +0300

    Celery: Using `os.environ`

--- docker/celery/celeryconfig.py
+++ docker/celery/celeryconfig.py
@@ -1,10 +1,13 @@
-BROKER_URL = 'redis://redis:6379/1'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/2'
+import os
+import json
+
+BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/1')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://redis:6379/2')
 CELERY_IMPORTS = ("tasks",)
 
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_ACCEPT_CONTENT = ['json']
+CELERY_ACCEPT_CONTENT = json.loads(os.environ.get('CELERY_ACCEPT_CONTENT', '["json"]'))
 
-CELERY_TIMEZONE = 'Europe/Moscow'
-CELERY_ENABLE_UTC = True
+CELERY_TIMEZONE = os.environ.get('CELERY_TIMEZONE', 'Europe/Moscow')
+CELERY_ENABLE_UTC = os.environ.get('CELERY_ENABLE_UTC', 'true') == 'true'
