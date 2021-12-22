f673f8371f59e76b4ea3847f797fd8459c754a37
intelrug <intelrug@gmail.com>
2021-12-22 04:21:01 +0300

    Abandon bootstrap styles

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,9 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
-import poststylus from 'poststylus';
-import postUse from 'postcss-use';
-import bootstrapStyl from 'bootstrap-styl';
 
 const ENV = process.env.NODE_ENV || 'development';
 const isDev = ENV === 'development';
@@ -44,14 +41,6 @@ const cssLoaderOptions = {
 
 const stylusLoaderOptions = {
   preferPathResolver: 'webpack',
-  use: [
-    bootstrapStyl(),
-    poststylus([
-      postUse({
-        modules: ['postcss-selector-namespace']
-      })
-    ])
-  ],
   compress: !isDev,
   sourceMap: true
 };
