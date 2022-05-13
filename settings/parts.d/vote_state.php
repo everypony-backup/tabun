57be6f0b089513d9c4f814ca2b2de6ae77ea6d2b
Vladimir Avchenov <vladimir.lsk.cool@gmail.com>
2022-05-14 01:32:36 +0300

    Показывать голоса за блоги только с 2022, May 10

--- settings/parts.d/vote_state.php
+++ settings/parts.d/vote_state.php
@@ -14,7 +14,7 @@ return [
         'date_sort' => SORT_ASC,
     ],
     'blog' => [
-        'as_date' => 0,
+        'as_date' => 1652133600,    // strtotime('2022-05-10') // 2022, May 10
         'oa_enable_level' => 6,
         'na_enable_level' => 6,     // Allow all allowed users to listen New Age
         'date_sort' => SORT_DESC,
